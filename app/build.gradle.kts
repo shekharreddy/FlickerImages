7f6e9e1923759216939a6f2402b35ec5d01fe76b
shekharnareddy <reddy.anddev@gmail.com>
2024-02-20 15:21:08 -0500

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,14 +82,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.5.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-//    debugImplementation("androidx.compose.ui:ui-tooling")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
     //Test
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     androidTestImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
